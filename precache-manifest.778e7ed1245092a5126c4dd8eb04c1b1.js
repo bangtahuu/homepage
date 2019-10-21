4f9817578c205fd36165e630a8471f0da23b49e5
Ta Huu Bang <bangth@MacBook-Pro-cua-Ta.local>
2019-10-21 22:22:20 +0700

    Updates

---
+++ precache-manifest.778e7ed1245092a5126c4dd8eb04c1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95ea7201c50c12b530705d0ce9f3774",
+    "url": "/hotel/index.html"
+  },
+  {
+    "revision": "cf62d972442ea2a7ed8b",
+    "url": "/hotel/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a93d76e625ef118816cb",
+    "url": "/hotel/static/js/2.109e17d6.chunk.js"
+  },
+  {
+    "revision": "cf62d972442ea2a7ed8b",
+    "url": "/hotel/static/js/main.c711b405.chunk.js"
+  },
+  {
+    "revision": "c4f876dd50c47355d1dd",
+    "url": "/hotel/static/js/runtime-main.e8220722.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hotel/static/media/logo.25bf045c.svg"
+  }
+]);
