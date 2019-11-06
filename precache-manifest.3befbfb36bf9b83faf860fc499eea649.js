07ba2ee0d58a90dd6f82843521e5eb5175a141b3
Ta Huu Bang <bangth@MacBook-Pro-cua-Ta.local>
2019-11-06 07:57:01 +0700

    Updates

---
+++ precache-manifest.3befbfb36bf9b83faf860fc499eea649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bde3ed2d8c3d44d25416369d4f1e613",
+    "url": "/hotel/index.html"
+  },
+  {
+    "revision": "9cd5c049afaacccd5127",
+    "url": "/hotel/static/css/main.2c9c0a14.chunk.css"
+  },
+  {
+    "revision": "7bdeeab595b5b1450dfa",
+    "url": "/hotel/static/js/2.9f68f71b.chunk.js"
+  },
+  {
+    "revision": "9cd5c049afaacccd5127",
+    "url": "/hotel/static/js/main.5324845a.chunk.js"
+  },
+  {
+    "revision": "c4f876dd50c47355d1dd",
+    "url": "/hotel/static/js/runtime-main.e8220722.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hotel/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hotel/precache-manifest.8691b1b4e2280249785ae43c4bf45e15.js"
+  "/hotel/precache-manifest.3befbfb36bf9b83faf860fc499eea649.js"
 );
 
 self.addEventListener('message', (event) => {
